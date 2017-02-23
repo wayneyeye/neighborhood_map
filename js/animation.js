a61581cec2c51402a9c5f15b54734d512d6e6873
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 08:23:12 -0600

    add side bar title

--- js/animation.js
+++ js/animation.js
@@ -5,6 +5,9 @@ function popSideBar(){
 	sidebar.style.display='block';
 	var search = document.getElementById('toggle-menu')
 	search.style.display='none';
+	var title = document.getElementById('title')
+	title.style.opacity=0;
+
 }
 //hides sidebar
 function hideSideBar(){
@@ -12,4 +15,6 @@ function hideSideBar(){
 	sidebar.style.display='none';
 	var search = document.getElementById('toggle-menu')
 	search.style.display='block';
+	var title = document.getElementById('title')
+	title.style.opacity=1;
 }
