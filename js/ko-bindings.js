469839d9ef7e8eeb7cbb37569a9304b933a833eb
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 10:57:52 -0600

    filter echoed modifed

--- js/ko-bindings.js
+++ js/ko-bindings.js
@@ -2,6 +2,9 @@ function SearchBarVM() {
 	var self = this;
 	self.storeArray = ko.observableArray(sb_data['businesses']);
 	self.filterText = ko.observable("");
+	self.formattedFilter = ko.computed(function() {
+        return self.filterText() ? "Filter = " + self.filterText() : "No Filter Applied"; //if price = 0 then return "None"       
+    });
 	// console.log(self.storeArray);
 }
 ko.applyBindings(new SearchBarVM());
