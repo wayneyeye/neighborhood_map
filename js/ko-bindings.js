d2c26b384d5152baca611bf06a676ce13c7b5d5b
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 15:35:52 -0600

    list animation

--- js/ko-bindings.js
+++ js/ko-bindings.js
@@ -6,39 +6,51 @@ function SearchBarVM() {
 	self.filterText = ko.observable("");
 	//formatted filter echo
 	self.formattedFilter = ko.computed(function() {
-        return self.filterText() ? "Filter = " + self.filterText() : "No Filter Applied! Try: 75080, Campbell, dallas, etc.";
-    });
-    //filtered store list
-    self.filteredArray = ko.computed(function() {
-    	self.fArray=new ko.observableArray();
-		self.storeArray().forEach(function(item){
+		return self.filterText() ? "Filter = " + self.filterText() : "No Filter Applied! Try: 75080, Campbell, dallas, etc.";
+	});
+	//filtered store list
+	self.filteredArray = ko.computed(function() {
+		self.fArray = new ko.observableArray();
+		self.storeArray().forEach(function(item) {
 			// console.log(item);	
-			if (item.store_address.toLowerCase().search(self.filterText().toLowerCase())!=-1){
+			if (item.store_address.toLowerCase().search(self.filterText().toLowerCase()) != -1) {
 				self.fArray.push(item);
-			}			
-		})      
-        return self.filterText() ? self.fArray(): self.storeArray(); 
-	  }
-	);
-	//count of results
-	self.resultCt = ko.computed(function() {return "Number of Results: "+self.filteredArray().length;});
-	//apply filter
-	self.applyFilter=function(){
-		// make filtered ID array and reduce complexity
-		var filteredID=[];
-		for (var j=0;j<self.filteredArray().length;j++){
-				filteredID.push(self.filteredArray()[j].id);
 			}
-		// console.log(filteredID);
+		})
+		return self.filterText() ? self.fArray() : self.storeArray();
+	});
+	//count of results
+	self.resultCt = ko.computed(function() {
+		return "Number of Results: " + self.filteredArray().length;
+	});
+	//click on results pops up interaction
+	self.popMarker = function(address) {
+		// console.log(address);
 		for (var i=0;i<markers.length;i++){
-				if (filteredID.indexOf(markers[i].id)!=-1){
-					markers[i].setMap(map);
+			if (address.id==markers[i].id){
+				if (markers[i].getAnimation() !== null) {
+					markers[i].setAnimation(null);
+				} else {
+					markers[i].setAnimation(google.maps.Animation.DROP);
 				}
-				else{
-					markers[i].setMap(null);				
-				}
+			}
+		}
+		}
+		//apply filter
+	self.applyFilter = function() {
+		// make filtered ID array and reduce complexity
+		var filteredID = [];
+		for (var j = 0; j < self.filteredArray().length; j++) {
+			filteredID.push(self.filteredArray()[j].id);
+		}
+		// console.log(filteredID);
+		for (var i = 0; i < markers.length; i++) {
+			if (filteredID.indexOf(markers[i].id) != -1) {
+				markers[i].setMap(map);
+			} else {
+				markers[i].setMap(null);
+			}
 		}
 	}
-
 }
 ko.applyBindings(new SearchBarVM());
