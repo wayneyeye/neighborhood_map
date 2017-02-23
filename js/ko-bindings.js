05b6d4299a0157d7ea0d4061d66c020c319234a4
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 10:42:49 -0600

    filter text echoed

--- js/ko-bindings.js
+++ js/ko-bindings.js
@@ -1,6 +1,7 @@
 function SearchBarVM() {
 	var self = this;
 	self.storeArray = ko.observableArray(sb_data['businesses']);
+	self.filterText = ko.observable("");
 	// console.log(self.storeArray);
 }
 ko.applyBindings(new SearchBarVM());
