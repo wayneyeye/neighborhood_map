7267073aa3428f899c4b4a95ec4f74285f1590ad
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 11:26:07 -0600

    filter function okay....

--- js/ko-bindings.js
+++ js/ko-bindings.js
@@ -3,8 +3,19 @@ function SearchBarVM() {
 	self.storeArray = ko.observableArray(sb_data['businesses']);
 	self.filterText = ko.observable("");
 	self.formattedFilter = ko.computed(function() {
-        return self.filterText() ? "Filter = " + self.filterText() : "No Filter Applied"; //if price = 0 then return "None"       
+        return self.filterText() ? "Filter = " + self.filterText() : "No Filter Applied! try Campbell, dallas, etc.";
     });
+    self.filteredArray = ko.computed(function() {
+    	self.fArray=new ko.observableArray();
+		self.storeArray().forEach(function(item){
+			// console.log(item);	
+			if (item.store_address.toLowerCase().search(self.filterText().toLowerCase())!=-1){
+				self.fArray.push(item);
+			}			
+		})      
+        return self.filterText() ? self.fArray(): self.storeArray(); 
+	  }
+	);
 	// console.log(self.storeArray);
 }
 ko.applyBindings(new SearchBarVM());
