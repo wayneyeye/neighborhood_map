89ad3a5004cbf4c94287f1ac4909ae3c44710465
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-23 08:00:00 -0600

    side animation done

--- js/animation.js
+++ js/animation.js
@@ -1,1 +1,15 @@
 //animations
+//pops sidebar
+function popSideBar(){
+	var sidebar = document.getElementById('side-bar');
+	sidebar.style.display='block';
+	var search = document.getElementById('toggle-menu')
+	search.style.display='none';
+}
+//hides sidebar
+function hideSideBar(){
+	var sidebar = document.getElementById('side-bar');
+	sidebar.style.display='none';
+	var search = document.getElementById('toggle-menu')
+	search.style.display='block';
+}
