5854d10ddc11d283a8314553721927b873300301
yewenhe0904 <yewenhe0904@gmail.com>
2017-02-22 18:50:59 -0600

    get yelp starbucks data

---
+++ api_data/request.py
@@ -0,0 +1,24 @@
+import io
+import json, jsonpickle
+from pprint import pprint
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+# read API keys
+with io.open('../secrets/config_secret.json') as cred:
+    creds = json.load(cred)
+    auth = Oauth1Authenticator(**creds)
+    client = Client(auth)
+
+params = {
+    'term': 'Starbucks',
+    'lang': 'en',
+    'radius_filter' : 10000,
+    'category_filter' : 'coffee',
+    'limit' : 10
+}
+response = client.search('Richardson, TX',**params)
+response_json=jsonpickle.encode(response)
+f = open('starbucks.json', 'w')
+f.write(response_json)
+f.close()
